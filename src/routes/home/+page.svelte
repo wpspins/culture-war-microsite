437d17878d00021238637b2cfb861f8d6cb5b13e
donaldmk9core <donalmk9@gmail.com>
2024-08-15 04:23:14 +0300

    test home route

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,27 @@
+<div class="about_cont">
+    <h2>About</h2>
+    <div class="about_info">
+        <div class="about_text">
+            <p>
+                Kinsta allows you to{' '}
+                <a> href="https://kinsta.com/static-site-hosting/">
+                    host up to 100 static websites
+                </a>{' '}
+                for free. This can be done by pushing your code to your preferred Git provider
+                (Bitbucket, GitHub, or GitLab) and then deploying it to Kinsta.
+            </p>
+            <p>
+                As an alternative to Static Site Hosting, you can opt for deploying your
+                static site with Kinsta’s{' '}
+                <a> href="https://kinsta.com/application-hosting/">
+                    Application Hosting
+                </a>
+                , which provides greater hosting flexibility, a wider range of benefits,
+                and access to more robust features. For example, scalability, customized
+                deployment using a Dockerfile, and comprehensive analytics encompassing real-time
+                and historical data.
+            </p>
+        </div>
+        <iframe> width="560" height="315" src="https://www.youtube.com/embed/H7CNbsda8OA?si=40FGVlNvJ74_6hlj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen> </iframe>
+    </div>
+</div>
