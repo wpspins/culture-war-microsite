8980982a19f6e3b095149cb8a8378a137403f299
WP SPIN LLC <134645735+wpspins@users.noreply.github.com>
2024-08-15 20:23:28 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-node';
-// import adapter from '@sveltejs/adapter-static';
-import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+// import sveltePreprocess from 'svelte-preprocess';
 
 
 /** @type {import('@sveltejs/kit').Config} */
